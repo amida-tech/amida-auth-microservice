431e13a9d213a24bc169528015e7d14252cbc6af
Jacob Sachs <jacob.s.sachs@gmail.com>
2017-09-11 10:52:39 -0400

    Add scopes, scope modification, and scope validation

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import validate from 'express-validation';
+import guard from 'express-jwt-permissions';
+import passport from 'passport';
 import paramValidation from '../../config/param-validation';
 import userCtrl from '../controllers/user.controller';
 
 const router = express.Router(); // eslint-disable-line new-cap
+const permissions = guard({permissionsProperty: 'scopes'});
 
 router.route('/')
     /** GET /api/users - Get list of users */
@@ -13,7 +16,6 @@ router.route('/')
     .post(validate(paramValidation.createUser), userCtrl.create);
 
 router.route('/:userId')
-
     /** GET /api/users/:userId - Get user */
     .get(userCtrl.get)
 
@@ -22,6 +24,13 @@ router.route('/:userId')
 
     /** DELETE /api/users/:userId - Delete user */
     .delete(userCtrl.remove);
+
+router.route('/scopes/:userId')
+    /** PUT /api/users/scopes/:userId - Update user scopes */
+    .put(validate(paramValidation.updateUserScopes),
+         passport.authenticate('jwt', { session: false }),
+         permissions.check('admin'),
+         userCtrl.updateScopes);
 
 router.route('/me')
     .get(userCtrl.me);

ad47201ebfe837fb49b91f0756f60c6016e4cbfb
Elijah George <elijah@amida.com>
2017-10-25 18:48:15 -0400

    Deploy with packansiform

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "api" {
 
   filter {
     name   = "name"
-    values = ["api-boilerplate-${var.build_env}*"]
+    values = ["api-${var.service_name}-service-boilerplate-${var.build_env}*"]
   }
 
   owners = ["844297601570"]
@@ -20,6 +20,7 @@ resource "aws_launch_configuration" "launch_config" {
   instance_type   = "${var.instance_type}"
   key_name        = "${var.key_name}"
   security_groups = ["${aws_security_group.api_sg.id}"]
+  iam_instance_profile = "app-ec2-role"
 
   root_block_device {
     delete_on_termination = true
@@ -33,12 +34,19 @@ resource "aws_launch_configuration" "launch_config" {
 }
 
 resource "aws_security_group" "api_sg" {
-  name        = "api-boilerplate-security-group"
+  name        = "api-${var.service_name}-service-boilerplate-security-group"
   description = "SG for API boilerplate deployment"
 
   ingress {
     from_port   = 0
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = "${var.port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -81,7 +89,7 @@ resource "aws_security_group" "api_sg" {
 
 resource "aws_autoscaling_group" "main_asg" {
   # interpolate the LC into the ASG name so it always forces an update
-  name = "api-${var.build_env}-asg-${data.aws_ami.api.id}"
+  name = "api-${var.service_name}-service-${var.build_env}-asg-${data.aws_ami.api.id}"
 
   # We want this to explicitly depend on the launch config above
   depends_on = ["aws_launch_configuration.launch_config"]
@@ -110,7 +118,7 @@ resource "aws_autoscaling_group" "main_asg" {
 
   tag {
     key                 = "Name"
-    value               = "api-${var.build_env}"
+    value               = "api-${var.service_name}-service-${var.build_env}"
     propagate_at_launch = true
   }
 
@@ -120,12 +128,12 @@ resource "aws_autoscaling_group" "main_asg" {
 }
 
 resource "aws_elb" "api_lb" {
-  name               = "api-boilerplate-lb"
+  name               = "api-${var.service_name}-service-lb"
   availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
   security_groups    = ["${aws_security_group.api_sg.id}"]
 
   listener {
-    instance_port     = 80
+    instance_port     = "${var.port}"
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
@@ -174,7 +182,25 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high" {
   }
 
   alarm_description = "This metric monitors high ec2 cpu utilization"
-  alarm_actions     = ["${aws_autoscaling_policy.scale_up.arn}"]
+  alarm_actions     = ["${aws_autoscaling_policy.scale_up.arn}", "arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_very_high" {
+  alarm_name          = "${aws_autoscaling_group.main_asg.name}-cpu-very-high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "90"
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.main_asg.name}"
+  }
+
+  alarm_description = "This metric monitors very high ec2 cpu utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.scale_up.arn}", "arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
 }
 
 resource "aws_autoscaling_policy" "scale_down" {
@@ -200,41 +226,123 @@ resource "aws_cloudwatch_metric_alarm" "cpu_low" {
   }
 
   alarm_description = "This metric monitors low ec2 cpu utilization"
-  alarm_actions     = ["${aws_autoscaling_policy.scale_down.arn}"]
+  alarm_actions     = ["${aws_autoscaling_policy.scale_down.arn}", "arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk_low" {
+  alarm_name          = "${aws_autoscaling_group.main_asg.name}-disk-low"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "DiskSpaceUtilization"
+  namespace           = "System/Linux"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "10"
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.main_asg.name}"
+    Filesystem = "/dev/xvda1"
+    MountPath = "/"
+  }
+
+  alarm_description = "This metric monitors low disk space utilization"
+  alarm_actions     = ["arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk_high" {
+  alarm_name          = "${aws_autoscaling_group.main_asg.name}-disk-high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "DiskSpaceUtilization"
+  namespace           = "System/Linux"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.main_asg.name}"
+    Filesystem = "/dev/xvda1"
+    MountPath = "/"
+  }
+
+  alarm_description = "This metric monitors high disk space utilization"
+  alarm_actions     = ["arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk_very_high" {
+  alarm_name          = "${aws_autoscaling_group.main_asg.name}-disk-very-high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "DiskSpaceUtilization"
+  namespace           = "System/Linux"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "90"
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.main_asg.name}"
+    Filesystem = "/dev/xvda1"
+    MountPath = "/"
+  }
+
+  alarm_description = "This metric monitors high disk space utilization"
+  alarm_actions     = ["arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "surge-que-length-high" {
+  alarm_name  = "${aws_elb.api_lb.name}-surge-que-length-high"
+  namespace   = "AWS/ELB"
+  metric_name = "SurgeQueueLength"
+
+  dimensions = {
+    LoadBalancerName = "${aws_elb.api_lb.name}"
+  }
+
+  statistic           = "Sum"
+  period              = 60
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "512"
+  evaluation_periods  = 2
+  alarm_description = "This metric monitors elb excess surge que length"
+  alarm_actions     = ["${aws_autoscaling_policy.scale_up.arn}", "arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "surge-que-length-low" {
+  alarm_name  = "${aws_elb.api_lb.name}-surge-que-length-low"
+  namespace   = "AWS/ELB"
+  metric_name = "SurgeQueueLength"
+
+  dimensions = {
+    LoadBalancerName = "${aws_elb.api_lb.name}"
+  }
+
+  statistic           = "Sum"
+  period              = 60
+  comparison_operator = "LessThanOrEqualToThreshold"
+  threshold           = "512"
+  evaluation_periods  = 2
+  alarm_description = "This metric monitors elb reduced surge que length"
+  alarm_actions     = ["${aws_autoscaling_policy.scale_down.arn}", "arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "ELB-5XX" {
+  alarm_name  = "${aws_elb.api_lb.name}-ELB-5XX"
+  namespace   = "AWS/ELB"
+  metric_name = "HTTPCode_ELB_5XX"
+
+  dimensions = {
+    LoadBalancerName = "${aws_elb.api_lb.name}"
+  }
+
+  statistic           = "Sum"
+  period              = 60
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "1"
+  evaluation_periods  = 2
+  alarm_description = "This metric monitors the presence of healthy instances and absence of request spill overs"
+  alarm_actions     = ["arn:aws:sns:us-west-2:844297601570:ops_team_alerts"]
 }
 
 variable "aws_region" {
   default = "us-west-2"
 }
-
-variable "instance_type" {}
-
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
-
-variable "key_name" {
-  description = "The SSH public key name (in EC2 key-pairs) to be injected into instances"
-}
-
-variable "asg_maximum_number_of_instances" {
-  description = "The maximum number of instances the ASG should maintain"
-}
-
-variable "asg_minimum_number_of_instances" {
-  description = "The minimum number of instances the ASG should maintain"
-}
-
-variable "health_check_grace_period" {
-  description = "Number of seconds for a health check to time out"
-}
-
-variable "health_check_type" {
-  description = "The health check used by the ASG to determine health"
-}
-
-variable "availability_zones" {
-  description = "A comma seperated list string of AZs the ASG will be associated with"
-}
-
-variable "build_env" {}

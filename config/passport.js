c8dc9dc85a2258f292c00af724c6c707c1cc378e
Jacob Sachs <jacob.s.sachs@gmail.com>
2018-03-30 13:19:09 -0400

    fix facebook breakage

--- config/passport.js
+++ config/passport.js
@@ -30,28 +30,28 @@ module.exports = (passport) => {
             .catch(err => done(err, false));
     });
 
-    const fbStrategy = new FacebookStrategy({
-        clientID: config.facebook.clientId,
-        clientSecret: config.facebook.clientSecret,
-        callbackURL: config.facebook.callbackUrl,
-        profileFields: ['email'],
-    }, (accessToken, refreshToken, profile, done) => {
-        const email = profile.emails[0].value;
-        User.findOrCreate({ where: {
-            username: email,
-            email,
-            provider: profile.provider,
-        } })
-        .spread((user) => {
-            if (user !== null) return done(null, user);
-            const err = new APIError('New facebook user not created', httpStatus.INTERNAL_SERVER_ERROR, true);
-            return done(err);
-        });
-    });
-
     passport.use(jwtStrategy);
 
     if (config.facebook.clientId) {
+        const fbStrategy = new FacebookStrategy({
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret,
+            callbackURL: config.facebook.callbackUrl,
+            profileFields: ['email'],
+        }, (accessToken, refreshToken, profile, done) => {
+            const email = profile.emails[0].value;
+            User.findOrCreate({ where: {
+                username: email,
+                email,
+                provider: profile.provider,
+            } })
+            .spread((user) => {
+                if (user !== null) return done(null, user);
+                const err = new APIError('New facebook user not created', httpStatus.INTERNAL_SERVER_ERROR, true);
+                return done(err);
+            });
+        });
+
         passport.use(fbStrategy);
     }
 };

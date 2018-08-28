3eed96899c66e0a659a026e3f7117611cf66d958
Data Brezack <databrezack@gmail.com>
2018-08-28 11:40:26 -0400

    Moving scripts out of index

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import pg from 'pg';
 import config from './config/config';
 import app from './config/express';
 /* eslint-disable no-unused-vars */
@@ -22,32 +21,24 @@ function startServer() {
     }
 }
 
-const conStringPri = `postgres://${config.postgres.user}:${config.postgres.passwd}@${config.postgres.host}:${config.postgres.port}/postgres`;
-pg.connect(conStringPri, (err, client, done) => { // eslint-disable-line no-unused-vars
-    // create the db and ignore any errors, for example if it already exists.
-    client.query(`CREATE DATABASE ${config.postgres.db}`, (err1) => { // eslint-disable-line no-unused-vars
-        client.end(); // close the connection
-      // db should exist now, initialize Sequelize
-        db.sequelize
-        .sync()
-        .then(() => {
-            db.User.count().then((total) => {
-                if (total === 0) {
-                    logger.info('Admin user not found. Creating.');
-                    const adminUser = Object.assign({}, config.adminUser);
-                    adminUser.password = passGenerator();
-                    logger.info(`Admin user password: ${adminUser.password}`);
-                    db.User.build(adminUser).save();
-                }
-            });
-        })
-        .then(startServer)
-        .catch((err2) => {
-            if (err2) debug('An error occured %j', err2);
-            else debug('Database synchronized');
-        });
-    });
-});
+db.sequelize
+  .sync()
+  .then(() => {
+      db.User.count().then((total) => {
+          if (total === 0) {
+              logger.info('Admin user not found. Creating.');
+              const adminUser = Object.assign({}, config.adminUser);
+              adminUser.password = passGenerator();
+              logger.info(`Admin user password: ${adminUser.password}`);
+              db.User.build(adminUser).save();
+          }
+      });
+  })
+  .then(startServer)
+  .catch((err) => {
+      if (err) debug('An error occured %j', err);
+      else debug('Database synchronized');
+  });
 
 
 export default app;

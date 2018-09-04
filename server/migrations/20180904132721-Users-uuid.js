c6975a5d519bbdaff99cb1605d4a45432d5492f4
Nick McCally <nick@amida.com>
2018-09-04 15:07:47 -0400

    migration fix

--- server/migrations/20180904132721-Users-uuid.js
+++ server/migrations/20180904132721-Users-uuid.js
@@ -1,24 +1,22 @@
 
 module.exports = {
-    // up: function(queryInterface, Sequelize) {
-    up: (queryInterface, Sequelize, done) => {
+    up: function (queryInterface, Sequelize) {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
       */
-
-        queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;').then(results => queryInterface.addColumn(
-    				'Users',
-    				'uuid',
-    				{
-    					type: Sequelize.UUID,
-    					defaultValue: Sequelize.literal('gen_random_uuid()'),
-    					allowNull: false,
-                unique: true,
-                primaryKey: true,
-    				}
-    			))
-        .nodeify(done);
+        return queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;').then(function() {
+            return queryInterface.addColumn('Users','uuid',
+            {
+                  type: Sequelize.UUID,
+        					defaultValue: Sequelize.literal('gen_random_uuid()'),
+        					allowNull: false,
+                  unique: true,
+                  primaryKey: true,
+              });
+        });
     },
-    down: (queryInterface, Sequelize, done) => queryInterface.removeColumn('Users', 'uuid').nodeify(done),
+    down: function (queryInterface, Sequelize) {
+    	 return queryInterface.removeColumn('Users', 'uuid');
+    }
 };

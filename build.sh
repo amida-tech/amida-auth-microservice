a848cf794f4e6e62745fc20a5143c6e2f24c3bbb
Grant Orndorff <grant@amida.com>
2018-09-18 11:41:11 -0400

    build.sh is working?

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # Setup temp directories
 rm -rf build node_modules
-mkdir build
-INSTALL_DIR=$(mktemp -d build/XXXX)
+INSTALL_DIR=build
+mkdir -p $INSTALL_DIR
 
 POSTGRES_SERVER_RPM=postgresql96-server-9.6.10-1PGDG.rhel7.x86_64.rpm
 POSTGRES_SERVER_URL=https://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/$POSTGRES_SERVER_RPM
@@ -31,7 +31,7 @@ rpm2cpio $POSTGRES_LIB_RPM | cpio -di
 export LD_LIBRARY_PATH=$PWD/usr/pgsql-9.6/lib/:$LD_LIBRARY_PATH
 export PATH=$PWD/usr/pgsql-9.6/bin/:$PATH
 
-# # Node
+# Node
 wget -q $NODE_URL
 tar -xf $NODE_TAR_NAME
 export PATH=$PWD/$NODE_DIR_NAME/bin:$PATH
@@ -43,11 +43,12 @@ export PATH=$PWD/node_modules/yarn/bin:$PATH
 cd -
 
 # Start Postgres
-initdb -D $POSTGRES_DATA_DIR -U amida
+initdb -D $POSTGRES_DATA_DIR -U amida_auth_microservice
 pg_ctl -D $POSTGRES_DATA_DIR -o "-h 127.0.0.1 -k $PWD/$POSTGRES_SOCK_DIR" start
 
 yarn
 cp .env.example .env.test
+export AUTH_SERVICE_REFRESH_TOKEN_ENABLED=true
 yarn test
 
 # Cleanup

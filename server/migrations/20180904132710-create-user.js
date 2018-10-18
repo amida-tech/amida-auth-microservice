cc48ca553f11dbf5fa33bdbd0c5e7b00f689d7d6
Nick McCally <nick@amida.com>
2018-10-18 19:23:52 -0400

    add table creation scripts

--- server/migrations/20180904132710-create-user.js
+++ server/migrations/20180904132710-create-user.js
@@ -1,58 +1,54 @@
-'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-          type: Sequelize.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-      },
-      username: {
-          type: Sequelize.STRING,
-          unique: true,
-          allowNull: false,
-      },
-      email: {
-          type: Sequelize.STRING,
-          unique: true,
-          allowNull: false,
-      },
-      password: {
-          type: Sequelize.STRING(512),
-      },
-      salt: {
-          type: Sequelize.STRING,
-      },
-      scopes: {
-          type: Sequelize.ARRAY(Sequelize.STRING),
-          defaultValue: [''],
-      },
-      refreshToken: {
-          type: Sequelize.STRING,
-      },
-      resetToken: {
-          type: Sequelize.STRING,
-      },
-      resetExpires: {
-          type: Sequelize.DATE,
-      },
-      provider: {
-          type: Sequelize.STRING,
-          allowNull: true,
-      },
-      createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('NOW()'),
-      },
-      updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('NOW()'),
-      },
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+    up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: Sequelize.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        password: {
+            type: Sequelize.STRING(512),
+        },
+        salt: {
+            type: Sequelize.STRING,
+        },
+        scopes: {
+            type: Sequelize.ARRAY(Sequelize.STRING),
+            defaultValue: [''],
+        },
+        refreshToken: {
+            type: Sequelize.STRING,
+        },
+        resetToken: {
+            type: Sequelize.STRING,
+        },
+        resetExpires: {
+            type: Sequelize.DATE,
+        },
+        provider: {
+            type: Sequelize.STRING,
+            allowNull: true,
+        },
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.literal('NOW()'),
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.literal('NOW()'),
+        },
+    }),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
 };
--- server/migrations/20180904132711-create-refreshToken.js
+++ server/migrations/20180904132711-create-refreshToken.js
@@ -1,30 +1,26 @@
-'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('refreshToken', {
-      id: {
-          type: Sequelize.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-      },
-      token: {
-          type: Sequelize.STRING,
-          unique: true,
-          allowNull: false,
-      },
-      createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('NOW()'),
-      },
-      updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('NOW()'),
-      },
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('refreshToken');
-  }
+    up: (queryInterface, Sequelize) => queryInterface.createTable('refreshToken', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        token: {
+            type: Sequelize.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.literal('NOW()'),
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.literal('NOW()'),
+        },
+    }),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('refreshToken'),
 };

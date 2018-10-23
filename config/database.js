46cbf18a0e20cd66ec9f8bae823cd5b0cf1a945f
Aaron Houlihan <aaron@amida.com>
2018-10-23 12:10:35 -0400

    config/database.js SSL for DB migration

--- config/database.js
+++ config/database.js
@@ -1,14 +1,26 @@
 const postgres = require('./config.js').postgres;
 
-const config = {
+let config = {
     username: postgres.user,
     password: postgres.password,
     database: postgres.db,
     host: postgres.host,
+    port: postgres.port,
     dialect: 'postgres',
     migrationStorageTableName: 'sequelize_meta',
     logging: true,
 };
+
+if (postgres.sslEnabled) {
+    config.ssl = postgres.sslEnabled;
+    if (postgres.sslCaCert) {
+        config.dialectOptions = {
+            ssl: {
+                ca: postgres.sslCaCert,
+            },
+        };
+    }
+}
 
 module.exports = {
     development: config,

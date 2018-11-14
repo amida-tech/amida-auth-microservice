f211267ee67e9e106bb490b09db4fff699a2be6d
Nick McCally <nick@amida.com>
2018-11-14 14:15:39 -0500

    add code to remove uuid from refreshToken when migration is run initially

--- server/migrations/20181114172213-userId-refreshToken.js
+++ server/migrations/20181114172213-userId-refreshToken.js
@@ -5,7 +5,7 @@ module.exports = {
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
       */
-        return queryInterface.sequelize.query('ALTER TABLE "refreshToken" ADD COLUMN IF NOT EXISTS "userId" INTEGER;');
+        return queryInterface.sequelize.query('ALTER TABLE "refreshToken" ADD COLUMN IF NOT EXISTS "userId" INTEGER; ALTER TABLE "refreshToken" DROP COLUMN IF EXISTS "uuid";');
     },
     down(queryInterface) {
         return true;

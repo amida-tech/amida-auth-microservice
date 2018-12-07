cda40bda79654c7a025a5ed0590b50e19b93c3aa
jmfadeley@gmail.com <jmfadeley@gmail.com>
2018-12-07 15:59:41 -0500

    [DEVOPS-365] Changes to the create and delete db scripts to make the commentary more valuable.

--- create_db.js
+++ create_db.js
@@ -5,17 +5,17 @@ const conStringPri = `postgres://${config.postgres.user}:${config.postgres.passw
 
 pg.connect(conStringPri, (err, client, done) => { // eslint-disable-line no-unused-vars
     if (err) {
-        console.log('error creaing database', err);
+        console.log('Error creating database:', err);
         process.exit(1);
     }
     // create the db and ignore any errors, for example if it already exists.
     client.query(`CREATE DATABASE "${config.postgres.db}";`, (err1) => { // eslint-disable-line no-unused-vars
         // If in test mode and a database exists don't let it get created
         if (err1) {
-            console.log(`Database Creation Failed. Please check to see if the database "${config.postgres.db}" already exists if so please delete it`);
+            console.log(`Database Creation Failed. Please check to see if the database "${config.postgres.db}" already exists if so please delete it.`);
             process.exit(1);
         } else {
-            console.log('Database Creation Succeeded.');
+            console.log('Database Creation Succeeded:', config.postgres.db);
             process.exit(0);
         }
     });

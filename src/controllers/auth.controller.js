68c2c54467323f596fd81f3b008db8920817b5e9
Jon Berry <jon@jonberry.co>
2019-04-15 14:19:12 -0400

    minor security fix for logical operator on requesting a password token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -189,7 +189,7 @@ function resetToken(req, res, next) {
     }
     // once the user promise resolves, send the token or an error
     Promise.join(user, (userResult) => {
-        if(config.requireVerificaiton || config.requireSecureVerificaiton && !userResult.isVerified()) {
+        if((config.requireVerificaiton || config.requireSecureVerificaiton) && !userResult.isVerified()) {
             const err = new APIError('User Not Verified', 'INVALID_EMAIL', httpStatus.BAD_REQUEST, true);
             return next(err);
         }
